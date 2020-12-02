1c1f0e6999692576a97c4afa937d5f306f0da871
Junior Criste <informaticode@gmail.com>
2020-12-01 23:09:22 -0300

    add questions in test

---
+++ pages/a7x/teste.php
@@ -0,0 +1,252 @@
+<?php
+session_start();
+include('verificaaut.php');
+?>
+<DOCTYPE html>
+<html lang="pt-br">
+<head>
+<title> Ambiente Virtual</title>
+<link href="../estilo/frame.css" rel="stylesheet">
+<link href="../estilo/icones.css" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Montserrat|Staatliches&display=swap' rel='stylesheet'/>
+</head>
+
+<body>
+<div id="nomeaula">Teste Seus Conhecimentos!</div>
+<div id="descaula">
+Agora você vai responder questões para obter a nota minima de 80%, assim obtendo o certificado.
+    <br />
+    <br />
+    <div class="questao"> <form>
+    <b>1. Supondo que você é a vítima em uma cena de ação, você está sendo amedrontado. Qual o plano?</b> 
+    <br />
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op1">
+    Contra Plongée
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op2">
+    Over the Shoulder
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op3">
+    Plongeé
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op4">
+    Americano
+    <br />
+    </div>
+
+    <br />  </form>
+    <div class="questao"> <form>
+    <b>2. Qual o nome do plano que representa partes do corpo em uma cena?</b> 
+    <br />
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op1">
+    Big Close Up
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op2">
+    Over the Shoulder
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op3">
+    Plongeé
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op4">
+    Close Up
+    <br />
+    </div>
+
+    <br />  </form>
+    <div class="questao"> <form>
+    <b>3. O personagem começou a andar pelo cenário, o cinegrafista o seguiu. Qual plano abaixo melhor se enquadra?</b> 
+    <br />
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op1">
+    Perseguição
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op2">
+    Sincronia
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op3">
+    Improviso
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op4">
+    Sequência
+    <br />
+    </div>
+
+
+    <br />  </form>
+    <div class="questao"> <form>
+    <b>4. O jovem corria, mas não o suficiente! Um tiro acertou seu peito, sua mãe de longe avistou tudo. Seu olhar de tristeza não pode ser escondido. Naquele mesmo momento, a câmera ia se fechando cada vez mais no rosto triste daquela mulher. Qual o nome desse efeito feito pelo cinegrafista?</b> 
+    <br />
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op1">
+    Fechamento
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op2">
+    Abertura
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op3">
+    Zoom Out
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op4">
+    Zoom In
+    <br />
+    </div>
+
+
+    <br />  </form>
+    <div class="questao"> <form>
+    <b>5. Uma fila enorme. Um cinegrafista parado. Ele precisa mostrar todos que estão na fila, mesmo não podendo andar pelo ambiente. Então decide mover o eixo da câmera, saindo da esquerda para a direita, pegando cada pessoa ali. Qual o nome desse movimento?</b> 
+    <br />
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op1">
+    Tilt
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op2">
+    Panorâmica
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op3">
+    Lateral Subjetivo
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op4">
+    Over the Shoulder
+    <br />
+    </div>
+
+
+
+
+    <br />  </form>
+    <div class="questao"> <form>
+    <b>9. Plano Subjetivo é...</b> 
+    <br />
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op1">
+    um plano onde a câmera pega do chão para o rosto da pessoa.
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op2">
+    um plano onde o câmera filma do alto para baixo.
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op3">
+    um plano onde o câmera filma alguém por trás.
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op4">
+    uma cena de ação, onde o câmera corre para filmar cada detalhe, fazendo a pessoa que assiste se sentir no ambiente.
+    <br />
+    </div>
+
+
+
+
+    <br />  </form>
+    <div class="questao"> <form>
+    <b>7. São pequenos acessórios que são presos a roupa, leves e recatados. Servem para captar o áudio.</b> 
+    <br />
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op1">
+    Autofalante
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op2">
+    Microfone
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op3">
+    Gravador
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op4">
+    Lapela
+    <br />
+    </div>
+
+
+
+
+    <br />  </form>
+    <div class="questao"> <form>
+    <b>8. Um ótimo programa gratuito para editar o áudio e remover ruídos é o...</b> 
+    <br />
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op1">
+    Kdenlive
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op2">
+    Paint
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op3">
+    Audacity
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op4">
+    Virtual DJ
+    <br />
+    </div>
+
+
+
+
+    <br />  </form>
+    <div class="questao"> <form>
+    <b>6. Fábio faz faculdade de jornalismo e decide criar um canal no YouTube para ir treinando seus aprendizados, mas seu salário não compra uma câmera, o que ele deve fazer?</b> 
+    <br />
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op1">
+    Desistir
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op2">
+    Esperar até comprar uma câmera
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op3">
+    Gravar com o próprio smartphone em um ambiente silêncioso e claro
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op4">
+    Alugar um equipamento
+    <br />
+    </div>
+
+
+
+
+    <br />  </form>
+    <div class="questao"> <form>
+    <b>10. Adobe Premiere e Sony Vegas são grandes editores de vídeos, mas não são acessíveis a todos os bolsos, nem a todos os sistemas. Por outro lado existe as equipes do software livre que pregam a liberdade de software, para uso, download e distribuição. Qual dos abaixos são editores de vídeos gratuitos?</b> 
+    <br />
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op1">
+    Shotcut e Kdenlive
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op2">
+    Shotcut, Davinci e 4K Video
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op3">
+    Sony Vegas, Adobe Premiere e Shotcut
+    <br />
+
+    <INPUT TYPE="RADIO" NAME="OPCAO" VALUE="op4">
+    Kdenlive e Wondershare
+    <br />
+    </div>
+
+
+
+
+
+
+</body>
+</html>
